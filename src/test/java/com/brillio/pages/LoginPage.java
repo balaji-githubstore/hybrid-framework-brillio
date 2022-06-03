598573e53bb5b289708c745e609e155ac549a3e2
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-06-03 11:01:09 +0530

    page object created for loginpage, mainpage

--- src/test/java/com/brillio/pages/LoginPage.java
+++ src/test/java/com/brillio/pages/LoginPage.java
@@ -5,33 +5,39 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class LoginPage {
-
+	private static By usernameLocator=By.id("authUser");
+	private static By passwordLocator=By.id("clearPass");
+	private static By languageLocator=By.xpath("//select[@name='languageChoice']");
+	private static By loginLocator=By.cssSelector("#login-button");
+	private static By ackLicCertLocator=By.partialLinkText("Acknowledgments");
+	private static By errorLocator=By.xpath("//div[contains(text(),'Invalid')]");
+	
 	public static void enterUsername(WebDriver driver, String username) {
-		driver.findElement(By.id("authUser")).sendKeys(username);
+		driver.findElement(usernameLocator).sendKeys(username);
 	}
 
 	public static void enterPassword(WebDriver driver, String password) {
-		driver.findElement(By.id("clearPass")).sendKeys(password);
+		driver.findElement(passwordLocator).sendKeys(password);
 	}
 	
 	public static void selectLanguageByText(WebDriver driver,String text)
 	{
-		Select selectLan=new Select(driver.findElement(By.xpath("//select[@name='languageChoice']")));
+		Select selectLan=new Select(driver.findElement(languageLocator));
 		selectLan.selectByVisibleText(text);
 	}
 	
 	public static void clickOnLogin(WebDriver driver)
 	{
-		driver.findElement(By.cssSelector("#login-button")).click();
+		driver.findElement(loginLocator).click();
 	}
 	
 	public static void clickOnAcknowledgmentsLicensingAndCertification(WebDriver driver)
 	{
-		driver.findElement(By.partialLinkText("Acknowledgments")).click();
+		driver.findElement(ackLicCertLocator).click();
 	}
 	
 	public static String getInvalidErrorMessage(WebDriver driver)
 	{
-		return driver.findElement(By.xpath("//div[contains(text(),'Invalid')]")).getText().strip();
+		return driver.findElement(errorLocator).getText().strip();
 	}
 }
--- src/test/java/com/brillio/pages/MainPage.java
+++ src/test/java/com/brillio/pages/MainPage.java
@@ -9,15 +9,21 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 //handle all the menu - openemr dashboard page
 public class MainPage {
+	private static By patientLocator=By.xpath("//div[text()='Patient']");
 	
 	public static void waitForPresenceOfPatientMenu(WebDriver driver)
 	{
 		WebDriverWait wait =new WebDriverWait(driver, Duration.ofSeconds(50));
-		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[text()='Patient']")));
+		wait.until(ExpectedConditions.presenceOfElementLocated(patientLocator));
 	}
 
 	public static String getMainPageTitle(WebDriver driver)
 	{
 		return  driver.getTitle();
+	}
+	
+	public static void clickOnPatient(WebDriver driver)
+	{
+		driver.findElement(patientLocator).click();
 	}
 }
